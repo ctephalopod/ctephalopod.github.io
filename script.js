9e5441707b9989b0d9cfcb714baddd14512334d8
ctephalopod <ctephalopod@gmail.com>
2023-03-20 10:04:15 -0500

    added titles to svg paths

--- script.js
+++ script.js
@@ -1,38 +1,99 @@
-const svg = document.getElementById('Layer_2');
-const paths = svg.querySelectorAll('path');
-const entryPopups = document.querySelectorAll('.entry-popup');
-const entryCloses = document.querySelectorAll(".entry-close");
+// Country pop-up function //
+   const svg = document.getElementById('Layer_2');
+   const paths = svg.querySelectorAll('path');
+   const entryPopups = document.querySelectorAll('.entry-popup');
+   const entryCloses = document.querySelectorAll(".entry-close");
 
-paths.forEach((path) => {
-  path.addEventListener('click', function() {
-    const popupId = this.getAttribute('data-popup');
-    const popup = document.getElementById(popupId);
-    entryPopups.forEach((entryPopup) => {
-      if (entryPopup === popup) {
-        entryPopup.classList.add('visible');
-      } else {
-        entryPopup.classList.remove('visible');
-      }
-    });
-  });
-});
-
-entryCloses.forEach((entryClose) => {
-  entryClose.addEventListener("click", function() {
-    const entryPopup = this.closest('.entry-popup');
-    entryPopup.classList.remove("visible");
-  });
-});
-
-function showTitleOnHover(path) {
-   const title = path.getAttribute('title');
-   const tooltip = document.createElement('div');
-   tooltip.classList.add('tooltip');
-   tooltip.textContent = title;
-   path.addEventListener('mouseover', () => {
-     document.body.appendChild(tooltip);
+   paths.forEach((path) => {
+   path.addEventListener('click', function() {
+      const popupId = this.getAttribute('data-popup');
+      const popup = document.getElementById(popupId);
+      entryPopups.forEach((entryPopup) => {
+         if (entryPopup === popup) {
+         entryPopup.classList.add('visible');
+         } else {
+         entryPopup.classList.remove('visible');
+         }
+      });
    });
-   path.addEventListener('mouseout', () => {
-     tooltip.remove();
    });
- } 
+
+   entryCloses.forEach((entryClose) => {
+   entryClose.addEventListener("click", function() {
+      const entryPopup = this.closest('.entry-popup');
+      entryPopup.classList.remove("visible");
+   });
+   });
+
+// Tooltip hover function //
+   function addTooltipToPath(pathId) {
+      const path = document.getElementById(pathId);
+      const title = path.getAttribute('title');
+   
+      path.addEventListener('mouseenter', function(event) {
+      const tooltip = document.getElementById(`${pathId}-tooltip`);
+      tooltip.classList.add('visible');
+      });
+   
+      path.addEventListener('mousemove', function(event) {
+      const tooltip = document.getElementById(`${pathId}-tooltip`);
+      tooltip.style.top = event.pageY + 'px';
+      tooltip.style.left = event.pageX + 'px';
+      });
+   
+      path.addEventListener('mouseleave', function(event) {
+      const tooltip = document.getElementById(`${pathId}-tooltip`);
+      tooltip.classList.remove('visible');
+      });
+   }
+   // Add tooltips to paths
+      addTooltipToPath('Uruguay');
+      addTooltipToPath('Brazil');
+      addTooltipToPath('Falkland');
+      addTooltipToPath('Argentina');
+      addTooltipToPath('Chile');
+      addTooltipToPath('Australia');
+      addTooltipToPath('NewZealand');
+      addTooltipToPath('Norway');
+      addTooltipToPath('Denmark');
+      addTooltipToPath('Sweden');
+      addTooltipToPath('Finland');
+      addTooltipToPath('Russia');
+      addTooltipToPath('Greece');
+      addTooltipToPath('Belgium');
+      addTooltipToPath('Netherlands');
+      addTooltipToPath('Germany');
+      addTooltipToPath('Switzerland');
+      addTooltipToPath('Austria');
+      addTooltipToPath('Italy');
+      addTooltipToPath('France');
+      addTooltipToPath('Spain');
+      addTooltipToPath('UnitedKingdom');
+      addTooltipToPath('Liechtenstein');
+      addTooltipToPath('Monaco');
+      addTooltipToPath('Hope');
+      addTooltipToPath('Egypt');
+      addTooltipToPath('HongKong');
+      addTooltipToPath('China');
+      addTooltipToPath('Macau');
+      addTooltipToPath('Japan');
+      addTooltipToPath('Taiwan');
+      addTooltipToPath('Israel');
+      addTooltipToPath('Turkey');
+      addTooltipToPath('Idaho');
+      addTooltipToPath('Montana');
+      addTooltipToPath('Wyoming');
+      addTooltipToPath('NorthDakota');
+      addTooltipToPath('SouthDakota');
+      addTooltipToPath('Minnesota');
+      addTooltipToPath('Nebraska');
+      addTooltipToPath('Kansas');
+      addTooltipToPath('Missouri');
+      addTooltipToPath('Florida');
+      addTooltipToPath('Alaska');
+      addTooltipToPath('Hawaii');
+      addTooltipToPath('VirginIslands');
+      addTooltipToPath('PuertoRico');
+      addTooltipToPath('DominicanRepublic');
+      addTooltipToPath('Canada');
+   
