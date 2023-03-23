60811e4e8c327537389bebd5e48bc66ad937f473
ctephalopod <ctephalopod@gmail.com>
2023-03-23 10:53:46 -0500

    added dropdown

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-// Country pop-up function //
-   const svg = document.getElementById('Layer_2');
-   const paths = svg.querySelectorAll('path');
-   const entryPopups = document.querySelectorAll('.entry-popup');
-   const entryCloses = document.querySelectorAll(".entry-close");
+// // Country pop-up function //
+//    const svg = document.getElementById('Layer_2');
+//    const paths = svg.querySelectorAll('path');
+//    const entryPopups = document.querySelectorAll('.entry-popup');
+//    const entryCloses = document.querySelectorAll(".entry-close");
 
-   paths.forEach((path) => {
-   path.addEventListener('click', function() {
-      const popupId = this.getAttribute('data-popup');
-      const popup = document.getElementById(popupId);
-      entryPopups.forEach((entryPopup) => {
-         if (entryPopup === popup) {
-         entryPopup.classList.add('visible');
-         } else {
-         entryPopup.classList.remove('visible');
-         }
-      });
-   });
-   });
+//    paths.forEach((path) => {
+//    path.addEventListener('click', function() {
+//       const popupId = this.getAttribute('data-popup');
+//       const popup = document.getElementById(popupId);
+//       entryPopups.forEach((entryPopup) => {
+//          if (entryPopup === popup) {
+//          entryPopup.classList.add('visible');
+//          } else {
+//          entryPopup.classList.remove('visible');
+//          }
+//       });
+//    });
+//    });
 
-   entryCloses.forEach((entryClose) => {
-   entryClose.addEventListener("click", function() {
-      const entryPopup = this.closest('.entry-popup');
-      entryPopup.classList.remove("visible");
-   });
-   });
+//    entryCloses.forEach((entryClose) => {
+//    entryClose.addEventListener("click", function() {
+//       const entryPopup = this.closest('.entry-popup');
+//       entryPopup.classList.remove("visible");
+//    });
+//    });
 
 // Tooltip hover function //
    function addTooltipToPath(pathId) {
